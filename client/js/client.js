cdf10365255e438adb9d1284c9c6313dd8c726cc
Austin Schafer <acsmars@gmail.com>
2017-09-05 23:01:59 -0700

    Hardset socket address for UI local dev purposes. Begin testing react.

---
+++ client/js/client.js
@@ -0,0 +1,6 @@
+
+
+var socket = io("https://mafia.acsmars.com",{path: '/socket.io'}); // Hardset for now for localhost debug reasons
+socket.on('connectionConfirmation', function(msg){
+    console.log(msg);
+});
